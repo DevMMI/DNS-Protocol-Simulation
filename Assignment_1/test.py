aa2afee260d16d842b1baa2e0c6a45fe7960cbb0
Moham893 <Moham893@umn.edu>
2018-02-20 21:55:35 -0600

    Most of server completed

---
+++ Assignment_1/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys, threading, os, random
+import socket
+import subprocess
+import re
+
+def main():
+    try:
+        address = "206.190.39.42"
+        command = "ping -c 1 "+address
+        test = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = test.communicate()
+        result = out.split(" ")
+        for i in range(len(result)):
+            word = result[i]
+            pattern = 'time=[\d.]*'
+            match = re.match(pattern, word, flags=0)
+            if match:
+                break
+        print float(word.split("=")[1])
+    except:
+        print '0'
+        return float(0)
+    
+        
+main()
